1a6ad6d2313a7163728d9a434ccfdd250239394d
Kushal-Mangla <kushal.mangla@students.iiit.ac.in>
2024-11-13 03:49:49 +0530

    kushal

--- naming_server/handle_threads.c
+++ naming_server/handle_threads.c
@@ -1,5 +1,117 @@
 #include"headers.h"
 
+
+ss storage_server_list[100];
+int storage_server_count = 0;
+
+void handle_file_request(request req, int client_id)
+{
+    if(strstr(req->data, ".txt") == NULL && req->request_type != GET_FILE_INFO)
+    {
+        request r = (request)malloc(sizeof(st_request));
+        r->request_type = INVALID_FILETYPE;
+        strcpy(r->data, "Incompatible file opened!");
+        send(client_id, r, sizeof(st_request), 0);
+        return;
+    }
+    int file_found = 0;
+    int id = 0;
+    response r = (response)malloc(sizeof(st_response));
+    // search for the file in the storage servers
+    for(int i = 0; i < storage_server_count; i++)
+    {
+        // if(search(ss_list[i]->root, req->data) >= 0)
+        // {
+        //     file_found = 1;
+        //     id = i;
+        //     break;
+        // }
+    }
+    if(file_found)
+    {
+        r->response_type = FILE_NOT_FOUND;
+        strcpy(r->message, "File not found");
+        send(socket_arr[client_id], r, sizeof(st_request), 0);
+        return;
+    }
+    else
+    {
+        if(req->request_type == READ_FILE)
+        {
+            // read the file from the storage server
+            // send the file to the client
+        }
+        else if(req->request_type == WRITE_FILE)
+        {
+            // write the file to the storage server
+            // send the response to the client
+        }
+        else if(req->request_type == GET_FILE_INFO)
+        {
+            // get the file info from the storage server
+            // send the file info to the client
+        }
+        else if(req->request_type == STREAM_AUDIO)
+        {
+            // stream the audio file from the storage server
+            // send the audio file to the client
+        }
+    }
+}
+void handle_client_process(void *arg)
+{
+    // handle the client request
+    proc n = (proc)arg;
+    int client_id = n->client_id;
+    request req = (request)malloc(sizeof(st_request));
+    req->request_type = n->request_type;
+    strcpy(req->data, n->data);
+    if(req->request_type == READ_FILE || req->request_type == WRITE_FILE || req->request_type == GET_FILE_INFO || req->request_type == STREAM_AUDIO)
+    {
+       handle_file_request(req, client_id);
+    }
+}
+void client_handler(void *arg)
+{
+    int client_socket = *(int *)arg;
+    while(1)
+    {
+        request req = (request)malloc(sizeof(st_request));
+        int res = recv(client_socket, req, sizeof(st_request), 0);
+        if(res <= 0){
+            free(req);
+            perror("Error in receiving request from client");
+            exit(1);
+        }
+        int req_valid = 1;
+        if(req_valid)        
+        {
+            pthread_t process;
+            proc n = (proc)malloc(sizeof(req_process));
+            n->request_type = req->request_type;
+            strcpy(n->data, req->data);
+            n->client_id = client_socket;
+            pthread_create(&process, NULL, &handle_client_process, (void *)n);
+        }
+    }
+}
+
+void storage_server_handler(void *arg)
+{
+    int storage_server_id = *(int *)arg;
+    while(1)
+    {
+        request req = (request)malloc(sizeof(st_request));
+        int res = recv(storage_server_id, req, sizeof(st_request), 0);
+        if(res <= 0){
+            free(req);
+            perror("Error in receiving request from storage server");
+            exit(1);
+        }
+        int req_valid = 1;
+    }
+}
+
 void *work_handler(){
     // create a socket for the naming server
     int server_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -9,9 +121,10 @@ void *work_handler(){
     }
     // make the port reusable
     int opt = 1;
-    if(setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) == -1){
-        perror("Setsockopt failed");
-        exit(1);
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        close(server_socket);
+        exit(EXIT_FAILURE);
     }
     // bind the socket to the naming server's IP and port
     struct sockaddr_in server_addr;
@@ -47,12 +160,32 @@ void *work_handler(){
                 // recieve a message from the client or storage server to determine the type of connection
                 char buffer[1024] = {0};
                 read(socket_arr[i][0], buffer, 1024);
-                if(strcmp(buffer, "client") == 0){
+                if(strcmp(buffer, "client") == 0)
+                {
+                    // send a message to the client to confirm the connection 
+                    send(socket_arr[i][0], "Hello client server", strlen("Hello client server"), 0);
+                    socket_arr[i][1] = CLIENT_FLAG;
+
                     // create a new thread to handle the client request
                     pthread_t client_thread;
-                    pthread_create(&client_thread, NULL, &client_handler, (void *)i);
+                    pthread_create(&client_thread, NULL, &client_handler, (void *)&socket_arr[i][0]);
                 }
+
                 else if(strcmp(buffer, "storage_server") == 0){
+                    // send a message to the storage server to confirm the connection
+                    send(socket_arr[i][0], "Hello storage server", strlen("Hello storage_server"), 0);
+                    socket_arr[i][1] = STORAGE_FLAG;
+
+                    // recieve the storage server's IP address and port number 
+                    storage_server_info* ss_info = (storage_server_info *)malloc(sizeof(storage_server_info));
+                    recv(socket_arr[i][0], ss_info, sizeof(storage_server_info), 0);
+
+                    storage_server_list[storage_server_count] = ss_info;    
+                    storage_server_count++;
+
+                    
+
+
                     // create a new thread to handle the storage server request
                     pthread_t storage_server_thread;
                     pthread_create(&storage_server_thread, NULL, &storage_server_handler, (void *)i);
--- naming_server/headers.h
+++ naming_server/headers.h
@@ -28,15 +28,54 @@
 #define YELLOW_COLOR "\033[0;33m"
 
 #define MAX_CLIENTS 100
+#define MAX_DATA_LENGTH 100000
 #define NS_PORT 3000
 #define MAX_CONNECTIONS 120
 #define NS_IP "0.0.0.0"
 #define MAX_PATH_LEN  1024  
+#define MAX_REQUEST_LEN 1000
+#define STORAGE_FLAG 1
+#define CLIENT_FLAG 2
 #define MAX_FILES 25
+#define READ_FILE 1
+#define WRITE_FILE 2
+#define GET_FILE_INFO 3
+#define STREAM_AUDIO 4
+#define INVALID_FILETYPE 402
+#define FILE_NOT_FOUND 404
 
 extern int socket_arr[MAX_CONNECTIONS][2];
 
 void *work_handler();
+
+
+typedef struct st_request
+{
+    int request_type;
+    char data[MAX_REQUEST_LEN];
+} st_request;
+
+typedef st_request* request;
+
+typedef struct req_process
+{
+    int client_id;
+    int request_type;
+    char data[MAX_DATA_LENGTH];
+}req_process;
+
+
+typedef req_process* proc;
+
+
+typedef struct st_response
+{
+    int response_type;
+    char message[MAX_REQUEST_LEN];
+} st_response;
+
+typedef st_response* response;
+
 
 typedef struct storage_server{
     char IP_Addr[15];
@@ -44,7 +83,9 @@ typedef struct storage_server{
     int Client_Port;
     // Rem
 
-} storage_server;
+} storage_server_info;
+
+typedef storage_server_info* ss;
 
 typedef struct client{
     char IP_Addr;
