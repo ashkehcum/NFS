f821f786ceb9349a165f8ca086d61340d69bb9ba
Hardik1106 <hardik.chadha@research.iiit.ac.in>
2024-11-15 00:14:49 +0530

    Log Initialization 2

--- naming_server/log.c
+++ naming_server/log.c
@@ -13,9 +13,9 @@ void LogMessage(bool isClient, char* IP_Addr, int Port_No, st_request request, i
     char time_str[30];
     strftime(time_str, 30, "%Y-%m-%d %H:%M:%S", time_info);
     if (isClient) {
-        fprintf(log_file, "%s: Client %s:%d requested %d type %s %s %s %d\n", time_str, IP_Addr, Port_No, request.request_type, request.data, request.path, request.copytopath, response_code);
+        fprintf(log_file, "%s: Client %s:%d requested %d type %s %s %s %d\n", time_str, IP_Addr, Port_No, request.request_type, request.data, request.path, request.copy_to_path, response_code);
     } else {
-        fprintf(log_file, "%s: Storage Server %s:%d requested %s type %s %s %s %d\n", time_str, IP_Addr, Port_No, request.request_type, request.data, request.path, request.copytopath, response_code);
+        fprintf(log_file, "%s: Storage Server %s:%d requested %s type %s %s %s %d\n", time_str, IP_Addr, Port_No, request.request_type, request.data, request.path, request.copy_to_path, response_code);
     }
     fclose(log_file);
 }
